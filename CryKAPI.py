59fc63efd56990704c9235b6f5fa5b66650dd30a
Cosmin <cosmin.cruceanu2@gmail.com>
2023-01-29 19:07:06 +0200

    More routes, better security much wow

--- CryKAPI.py
+++ CryKAPI.py
@@ -1,14 +1,16 @@
 from authlib.integrations.flask_client import OAuth
-from flask import Flask, jsonify, request, send_file, url_for, session, Response, json, abort
-from google.auth.exceptions import MalformedError
+from flask import Flask, jsonify, request, session, Response, json, abort
+from google.auth.exceptions import MalformedError, InvalidValue
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
-from CryKDatabase import insert_user, find_account
+import CryKDatabase
 from config import GOOGLE_CLIENT_ID
 from emailer import send_register_mail
 from images import get_image, image_dict, get_response_image
 from models.Cryptocurrency import Cryptocurrency
+from models.Portfolio import Portfolio
+from models.Profile import Profile
 from models.users import User
 
 app = Flask(__name__)
@@ -26,17 +28,31 @@ oauth.register(
 )
 
 
+# <editor-fold desc="images routes">
 @app.route('/cryk/api/getImages', methods=['POST'])
 def get_crypto_image():
+    if not check_user_session():
+        abort(401)
+    if 'images' not in request.json:
+        abort(400)
     image_paths = []
-    payload = request.json
-    images = payload["images"]
+    images = request.json['images']
     for image in images:
         image_paths.append(get_image(image))
     encoded_images = {}
     for image, image_path in zip(images, image_paths):
         encoded_images[image] = get_response_image(image_path)
     return jsonify(encoded_images)
+
+
+@app.route('/cryk/api/cryptocurrencies/dictionary', methods=['GET'])
+def get_crypto_dictionary():
+    if not check_user_session():
+        abort(401)
+    return jsonify(image_dict)
+
+
+# </editor-fold>
 
 
 @app.route('/cryk/api/getVisualiaztionData', methods=['POST'])
@@ -48,19 +64,29 @@ def get_visualization_data():
     return jsonify(item_list)
 
 
+# <editor-fold desc="authentication routes">
 @app.route('/cryk/auth', methods=['POST'])
 def auth():
     try:
         response = Response()
         token = request.json['token']
         id_info = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+        print(id_info)
     except KeyError:
         return abort(400)
     except MalformedError:
         return abort(401)
+    except InvalidValue:
+        session.pop('user', None)
+        session.pop('id', None)
+        return abort(401)
     email = id_info['email']
     user = User(email=email)
-    id = insert_user(user)
+    id = CryKDatabase.insert_user(user)
+    CryKDatabase.insert_user_profile(Profile(user_id=id['$oid'],
+                                             firstname=id_info['given_name'],
+                                             lastname=id_info['family_name'],
+                                             email=id_info['email']))
     session['user'] = token
     session['id'] = id
     response.status = 200
@@ -71,7 +97,6 @@ def auth():
 
 @app.route('/cryk/api/logout', methods=['GET'])
 def logout():
-    print(session.get('id'))
     session.pop('user', None)
     session.pop('id', None)
     resp = Response()
@@ -87,7 +112,8 @@ def login():
     email = payload['email']
     password = payload['password']
     user = User(email=email, password=password)
-    id = find_account(user)
+    id = CryKDatabase.find_account(user)
+    session['id'] = id
     response.set_cookie("id", id['$oid'])
     response.data = json.dumps(id)
     response.status = 200 if id != -1 else 401
@@ -96,19 +122,85 @@ def login():
 
 @app.route('/cryk/api/register', methods=['POST'])
 def register():
-    payload = request.json
-    print(payload)
-    email = payload['email']
-    password = payload['password']
+    if 'email' not in request.json or 'password' not in request.json:
+        abort(400)
+    email = request.json['email']
+    password = request.json['password']
     new_user = User(email=email, password=password)
-    insert_user(new_user)
+    CryKDatabase.insert_user(new_user)
     send_register_mail(new_user)
     return jsonify(succes=True)
 
 
-@app.route('/cryk/api/cryptocurrencies/dictionary', methods=['GET'])
-def get_crypto_dictionary():
-    return jsonify(image_dict)
+# </editor-fold>
+
+# <editor-fold desc="portfolio routes">
+@app.route('/cryk/api/insertPortfolio', methods=['POST'])
+def insert_user_portfolio():
+    if not check_user_session():
+        abort(401)
+    if 'coins' not in request.json or any([False if coin in image_dict else True for coin in request.json['coins']]):
+        abort(400)
+    portfolio = Portfolio(user_id=session['id']['$oid'], coins=request.json['coins'])
+    CryKDatabase.insert_user_portfolio(portfolio)
+    return jsonify(200)
+
+
+@app.route('/cryk/api/getPortfolio')
+def get_user_portfolio():
+    if not check_user_session():
+        abort(401)
+    portfolio = CryKDatabase.get_user_portfolio(session['id']['$oid'])
+    return jsonify(portfolio)
+
+
+# </editor-fold>
+
+# <editor-fold desc="profile routes">
+@app.route('/cryk/api/getProfile')
+def get_user_profile():
+    if not check_user_session():
+        abort(401)
+    return jsonify(CryKDatabase.get_user_profile(session['id']['$oid']))
+
+
+@app.route('/cryk/api/insertProfile', methods=['POST'])
+def insert_user_profile():
+    if not check_user_session():
+        abort(401)
+    if 'firstname' not in request.json or 'lastname' not in request.json or 'email' not in request.json:
+        abort(400)
+    profile = Profile(firstname=request.json['firstname'],
+                      lastname=request.json['lastname'],
+                      email=request.json['email'],
+                      city=request.json['city'] if 'city' in request.json else "",
+                      country=request.json['country'] if 'country' in request.json else "",
+                      address=request.json['address'] if 'address' in request.json else "",
+                      about=request.json['about'] if 'about' in request.json else "",
+                      user_id=session['id']['$oid']
+                      )
+    if 'coins' in request.json and any([False if coin in image_dict else True for coin in request.json['coins']]):
+        abort(400)
+    portfolio = Portfolio(user_id=session['id']['$oid'],
+                          coins=request.json['coins'] if 'coins' in request.json else {})
+    CryKDatabase.insert_user_profile(profile)
+    CryKDatabase.insert_user_portfolio(portfolio)
+    return jsonify(200)
+
+
+# </editor-fold>
+def check_user_session():
+    if 'user' in session:
+        try:
+            id_token.verify_oauth2_token(session['user'], requests.Request(), GOOGLE_CLIENT_ID)
+        except MalformedError:
+            return False
+        return True
+    elif 'id' in session:
+        if not CryKDatabase.is_user_in_database(session['id']['$oid']):
+            return False
+        return True
+    return False
 
 
 if __name__ == '__main__':
--- ontology.py
+++ ontology.py
@@ -1,4 +1,11 @@
+import io
+import json
+from typing import Dict, Any
+
 import rdflib
+from rdflib import URIRef, Literal, BNode
+from rdflib.plugins.sparql.results.jsonresults import JSONResultSerializer
+from rdflib.query import ResultException
 
 from models.Cryptocurrency import Cryptocurrency
 
@@ -24,17 +31,20 @@ def get_cryptocurrencies_by_protocol_from_ontology(consensus):
                 ?x doacc:date-founded ?date_founded .
                 ?x doacc:incept ?incept .
                 ?x doacc:{consensus} ?consensus .
-                ?x doacc:premine ?premine  .
                 ?x doacc:protection-scheme ?protection_scheme .   
                 ?x doacc:source ?source .
                 ?x doacc:protocol ?protocol .   
                 ?x doacc:symbol ?symbol .
                 ?x doacc:total-coins ?total_coins .
-                ?x doacc:website ?website .
+                OPTIONAL {{
+                    ?x doacc:website ?website .
+                }}
+                OPTIONAL{{
+                        ?x doacc:premine ?premine .
+                }} .
             }}
     """
     )
-
     cryptocurrencies = []
     for cryptocurrency in result:
         cryptocurrencies.append(Cryptocurrency(name=cryptocurrency[0].toPython(),
@@ -46,13 +56,15 @@ def get_cryptocurrencies_by_protocol_from_ontology(consensus):
                                                if consensus == "pow" else "",
                                                proof_of_stake=cryptocurrency[5].toPython()
                                                if consensus == "pos" else "",
-                                               premine=cryptocurrency[6].toPython(),
-                                               protection_scheme=cryptocurrency[7].toPython(),
-                                               source=cryptocurrency[8].toPython(),
-                                               protocol=cryptocurrency[9].toPython(),
-                                               symbol=cryptocurrency[10].toPython(),
-                                               total_coins=cryptocurrency[11].toPython(),
-                                               website=cryptocurrency[12].toPython()
+                                               protection_scheme=cryptocurrency[6].toPython(),
+                                               source=cryptocurrency[7].toPython(),
+                                               protocol=cryptocurrency[8].toPython(),
+                                               symbol=cryptocurrency[9].toPython(),
+                                               total_coins=cryptocurrency[10].toPython(),
+                                               website=cryptocurrency[11].toPython()
+                                               if cryptocurrency[11] is not None else "",
+                                               premine=cryptocurrency[12].toPython()
+                                               if cryptocurrency[12] is not None else "",
                                                ))
     return cryptocurrencies
 
@@ -80,8 +92,10 @@ def get_cryptocurrency_details_from_ontology(identifier):
                     ?x doacc:protocol ?protocol .   
                     ?x doacc:symbol ?symbol .
                     ?x doacc:total-coins ?total_coins .
-                    ?x doacc:website ?website .
                     
+                    OPTIONAL {{
+                    ?x doacc:website ?website .
+                    }}
                     OPTIONAL{{
                         ?x doacc:pos ?pos .
                     }} .
@@ -96,7 +110,6 @@ def get_cryptocurrency_details_from_ontology(identifier):
                 }}
         """
     )
-
     cryptocurrencies = []
     for cryptocurrency in result:
         cryptocurrencies.append(Cryptocurrency(name=cryptocurrency[0].toPython(),
@@ -109,7 +122,8 @@ def get_cryptocurrency_details_from_ontology(identifier):
                                                protocol=cryptocurrency[7].toPython(),
                                                symbol=cryptocurrency[8].toPython(),
                                                total_coins=cryptocurrency[9].toPython(),
-                                               website=cryptocurrency[10].toPython(),
+                                               website=cryptocurrency[10].toPython()
+                                               if cryptocurrency[10] is not None else "",
                                                proof_of_stake=cryptocurrency[11].toPython()
                                                if cryptocurrency[11] is not None else "",
                                                proof_of_work=cryptocurrency[12].toPython()
@@ -118,3 +132,130 @@ def get_cryptocurrency_details_from_ontology(identifier):
                                                if cryptocurrency[13] is not None else "",
                                                ))
     return cryptocurrencies
+
+
+def get_cryptocurrencies_details_from_ontology(coins):
+    result = g.query(
+        f"""
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+                PREFIX ccy: <http://purl.org/net/bel-epa/ccy#>
+                PREFIX dc: <http://purl.org/dc/elements/1.1/>
+                PREFIX doacc: <http://purl.org/net/bel-epa/doacc#>
+
+                SELECT ?name ?description ?block_time ?date_founded ?incept
+                    ?protection_scheme
+                    ?source ?protocol ?symbol ?total_coins ?website 
+                    ?pos ?pow ?premine
+                WHERE {{
+                    ?x skos:prefLabel ?name .
+                    ?x dc:description ?description .
+                    ?x doacc:block-time ?block_time .
+                    ?x doacc:date-founded ?date_founded .
+                    ?x doacc:incept ?incept .
+                    ?x doacc:protection-scheme ?protection_scheme .   
+                    ?x doacc:source ?source .
+                    ?x doacc:protocol ?protocol .   
+                    ?x doacc:symbol ?symbol .
+                    ?x doacc:total-coins ?total_coins .
+                    
+                    Optional {{
+                        ?x doacc:website ?website .
+                    }}
+                    OPTIONAL{{
+                        ?x doacc:pos ?pos .
+                    }} .
+                    OPTIONAL{{
+                        ?x doacc:pow ?pow .
+                    }} .
+                    OPTIONAL{{
+                        ?x doacc:premine ?premine .
+                    }} .
+                    FILTER regex(?name, "^{build_query_parameter(coins)}$", "i")
+
+                }}
+        """
+    )
+    cryptocurrencies = []
+    for cryptocurrency in result:
+        cryptocurrencies.append(Cryptocurrency(name=cryptocurrency[0].toPython(),
+                                               description=cryptocurrency[1].toPython(),
+                                               block_time=cryptocurrency[2].toPython(),
+                                               date_founded=cryptocurrency[3].toPython(),
+                                               incept=cryptocurrency[4].toPython(),
+                                               protection_scheme=cryptocurrency[5].toPython(),
+                                               source=cryptocurrency[6].toPython(),
+                                               protocol=cryptocurrency[7].toPython(),
+                                               symbol=cryptocurrency[8].toPython(),
+                                               total_coins=cryptocurrency[9].toPython(),
+                                               website=cryptocurrency[10].toPython()
+                                               if cryptocurrency[10] is not None else "",
+                                               proof_of_stake=cryptocurrency[11].toPython()
+                                               if cryptocurrency[11] is not None else "",
+                                               proof_of_work=cryptocurrency[12].toPython()
+                                               if cryptocurrency[12] is not None else "",
+                                               premine=cryptocurrency[13].toPython()
+                                               if cryptocurrency[13] is not None else "",
+                                               ))
+    return cryptocurrencies
+
+
+def perform_query_on_ontology(query):
+    try:
+        result = g.query(query)
+        serialize(result)
+        return serialize(result)
+    except Exception as e:
+        print(e)
+    return 1
+
+
+def build_query_parameter(coins):
+    query = ''
+    coins = set(coins)
+    for coin in coins:
+        query += coin + '$|'
+    return query[:-1]
+
+
+def serialize(result):
+    res: Dict[str, Any] = {}
+    if result.type == "ASK":
+        res["head"] = {}
+        res["boolean"] = result.askAnswer
+    else:
+        res["results"] = {}
+        res["head"] = {}
+        res["head"]["vars"] = result.vars
+        res["results"]["bindings"] = [
+            binding_to_json(x) for x in result.bindings
+        ]
+    return res
+
+
+def binding_to_json(b):
+    res = {}
+    for var in b:
+        j = term_to_json(b[var])
+        if j is not None:
+            res[var] = term_to_json(b[var])
+    return res
+
+
+def term_to_json(term):
+    if isinstance(term, URIRef):
+        return {"type": "uri", "value": str(term)}
+    elif isinstance(term, Literal):
+        r = {"type": "literal", "value": str(term)}
+
+        if term.datatype is not None:
+            r["datatype"] = str(term.datatype)
+        if term.language is not None:
+            r["xml:lang"] = term.language
+        return r
+
+    elif isinstance(term, BNode):
+        return {"type": "bnode", "value": str(term)}
+    elif term is None:
+        return None
+    else:
+        raise ResultException("Unknown term type: %s (%s)" % (term, type(term)))
