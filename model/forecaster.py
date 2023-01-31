de724faad84f06f44251ef79e7c4159ed590bead
Theo <samson.theodor3699@gmail.com>
2023-01-31 11:32:00 +0200

    Second attempt at forecasting

--- model/forecaster.py
+++ model/forecaster.py
@@ -1,116 +1,68 @@
 # Data manipulation
 # ==============================================================================
-import matplotlib.pyplot as plt
+
 import pandas as pd
-from IPython.display import display
-from cryptocmd import CmcScraper
-
-plt.style.use('ggplot')
-
-# Bitcoin colors
-# ==============================================================================
-palette_btc = {'orange': '#f7931a',
-               'white': '#ffffff',
-               'gray': '#4d4d4d',
-               'blue': '#0d579b',
-               'green': '#329239'
-               }
-
-# Modelling and Forecasting
-# ==============================================================================
-from skforecast.ForecasterAutoreg import ForecasterAutoreg
-from skforecast.model_selection import backtesting_forecaster
-from lightgbm import LGBMRegressor
-from sklearn.metrics import mean_absolute_error
-
-# Data download
-# ==============================================================================
-
-# Scraper is initialized, symbol, start and end of download are included
-scraper = CmcScraper('BTC', '28-04-2013', '01-01-2022')
-
-# Transform collected data into a dataframe
-data = scraper.get_dataframe()
-data.sort_values(by='Date', ascending=True, inplace=True)
 
 pd.set_option('display.max_columns', None)
-display(data)
-pd.reset_option('display.max_columns')
+pd.set_option('display.max_rows', None)
 
-data['date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d %H:%M:%S')
-data = data.loc[:, ['date', 'Open', 'Close', 'High', 'Low']]
-data = data.rename({'Open': 'open', 'Close': 'close', 'High': 'high', 'Low': 'low'},
-                   axis=1)
-data = data.set_index('date')
-data = data.asfreq('D')
-data = data.sort_index()
+# Scraper is initialized, symbol, start and end of download are included
+# scraper = CmcScraper('BTC', '28-04-2013', '01-01-2022')
 
-# Selection of train-test dates
-# ==============================================================================
-start_train = '2020-01-01 00:00:00'
-end_train = '2021-06-30 23:59:59'
+# Transform collected data into a dataframe
+# btc = scraper.get_dataframe()
 
-print(f"Complete time series : {data.index.min()} --- {data.index.max()}  (n={len(data)})")
-print(
-    f"Train dates          : {data.loc[start_train:end_train].index.min()} --- {data.loc[start_train:end_train].index.max()}  (n={len(data.loc[start_train:end_train])})")
-print(
-    f"Test dates           : {data.loc[end_train:].index.min()} --- {data.loc[end_train:].index.max()}  (n={len(data.loc[end_train:])})")
+# btc.to_csv("btc.csv")
 
-# Random walk simulation
-# ==============================================================================
-df_rw = data[['close']].copy()
-df_rw['pred_close'] = df_rw['close'].shift(1)
+btc = pd.read_csv("btc.csv")
 
-# Test error
-# ==============================================================================
-y_true = df_rw.loc[end_train:, 'close']
-y_pred = df_rw.loc[end_train:, 'pred_close']
-metric = mean_absolute_error(y_true, y_pred)
+btc.sort_values(by=["Date"])
 
-print(f'Test error: {metric}')
-display(df_rw.loc[end_train:, ].head(4))
+btc.index = pd.to_datetime(btc['Date'], format='%Y-%m-%d')
 
-# Forecasters backtest with different lags
-# ==============================================================================
-lags = [1, 7, 30]
-metrics = []
-predictions_list = []
+import matplotlib.pyplot as plt
 
-for lag in lags:
-    # Create forecaster
-    forecaster = ForecasterAutoreg(
-        regressor=LGBMRegressor(random_state=123),
-        lags=lag,
-        transformer_y=None
-    )
+train = btc[btc.index < pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
+test = btc[btc.index > pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
 
-    # Backtest test data, 1 step
-    metric, predictions = backtesting_forecaster(
-        forecaster=forecaster,
-        y=data.loc[start_train:, 'close'],
-        initial_train_size=len(data.loc[start_train:end_train, 'close']),
-        fixed_train_size=True,
-        steps=1,
-        refit=True,
-        metric='mean_absolute_error',
-        verbose=False
-    )
+# plt.plot(train, color = "black")
+# plt.plot(test, color = "red")
+# plt.ylabel('BTC Price')
+# plt.xlabel('Date')
+# plt.xticks(rotation=45)
+# plt.title("Train/Test split for BTC Data")
+# plt.show()
 
-    metrics.append(metric)
-    predictions_list.append(predictions)
 
-# Plot
-# ==============================================================================
-fig, ax = plt.subplots(figsize=(11, 4.5))
-data.loc[end_train:, 'close'].plot(ax=ax, linewidth=2, label='Test')
+y = train['Open']
 
-# Plot test predictions for different lags
-for predictions, lag in zip(predictions_list, lags):
-    predictions = predictions.rename(columns={'pred': f'Prediction, {lag} lags'})
-    predictions.plot(ax=ax, linewidth=2)
+from statsmodels.tsa.arima.model import ARIMA
 
-ax.set_title('Close Price vs Predictions (Test Data)')
-ax.set_ylabel('Price (USD)')
-ax.legend();
+ARIMAmodel = ARIMA(y, order=(5, 4, 2))
+ARIMAmodel = ARIMAmodel.fit()
 
+y_pred = ARIMAmodel.get_forecast(len(test.index))
+print("test.index")
+print(test.index, len(test.index))
+y_pred_df = y_pred.conf_int(alpha=0.05)
+print("y_pred")
+print(y_pred)
+print("y_pred_df")
+print(y_pred_df)
+print(y_pred_df.index[0], y_pred_df.index[-1])
+y_pred_df["Predictions"] = ARIMAmodel.predict(start=y_pred_df.index[0], end=y_pred_df.index[-1])
+y_pred_df.index = test.index
+y_pred_out = y_pred_df["Predictions"]
+plt.plot(y_pred_out, color='Yellow', label='ARIMA Predictions')
+plt.legend()
 plt.show()
+
+print("forecast")
+print(ARIMAmodel.forecast(5))
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+arma_rmse = np.sqrt(mean_squared_error(test["Open"].values, y_pred_df["Predictions"]))
+print("RMSE: ", arma_rmse)
+print(train[-6:])
